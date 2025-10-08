ba7bc86601190fa0b3a17a8de030c1f7120c66ee
Alyssa Mitchell <ashinnickmitchell@gmail.com>
2025-10-07 23:10:50 -0600

    Update insertionSort.js

--- Arrays_and_Hashing/insertionSort.js
+++ Arrays_and_Hashing/insertionSort.js
@@ -1,9 +1,20 @@
-
 /**
- * @param {number[]} arr
- * @return {number[]}
+ * Pair class to store key-value pairs
  */
-function insertionSort(arr) {
-    // TODO: Implement insertion sort here
-    
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs) {}
 }
